38e49f77f12ba79510406007c3214423fd5481f4
Rajiv Kumar <rajivkumar@example.com>
2025-12-13 17:40:19 +0530

    test: configure Jest testing framework with MongoDB connection

---
+++ backend/jest.setup.js
@@ -0,0 +1,4 @@
+// Set test environment variables
+process.env.NODE_ENV = 'test';
+process.env.JWT_SECRET = 'testsecret123456789';
+process.env.MONGODB_URI = 'mongodb+srv://Rajivkumar:Rajivkumar@cluster0.5aesp.mongodb.net/sweet_shop_test?retryWrites=true&w=majority';
